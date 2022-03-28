9229dc7a1ff0c5f1b63987fe2f7a70d0a9bc0219
Robin Beck <robin.beck.we@gmail.com>
2022-03-28 15:07:48 +0200

    added basic interaction rig prefab and changed hands material to be compatible with urp

---
+++ Editor/Scripts/Movement/OVRContinuousMovementController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FHE.QuestInteractionPresets.Movement
+{
+
+    public class OVRContinuousMovementController : MonoBehaviour
+    {
+
+    }
+}
+
